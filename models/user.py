81b6d8cc78eee2d6a7ac0ab7879494a9e35c3d3c
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-12-23 18:21:42 +0530

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,15 @@
+from datetime import datetime
 
+
+def user_document(user_id: int, ref_by: int | None = None):
+    return {
+        "user_id": user_id,
+        "role": "free",          # free | premium | restricted | blocked | owner
+        "language": "en",
+        "ref_by": ref_by,
+        "created_at": datetime.utcnow(),
+        "stats": {
+            "joined_at": datetime.utcnow()
+        }
+    }
+  
